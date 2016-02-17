53f72e0cc350bcbf47e222ae49fff499cdcd7bab
dimabdc <support@titor.ru>
2016-02-17 19:19:17 +0700

    Add htaccess and url config

--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,17 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'enableStrictParsing' => false,
+            'rules' => [
+                '' => 'site/index',
+                '<_c:[\w\-]+>' => '<_c>/index',
+                '<_c:[\w\-]+>/<id:\d+>' => '<_c>/index',
+                '<_c:[\w\-]+>/<_a:[\w\-]+>' => '<_c>/<_a>',
+                '<_c:[\w\-]+>/<_a:[\w\-]+>/<id:\d+>' => '<_c>/<_a>',
+            ],
+        ],
     ],
 ];
--- environments/dev/backend/config/main-local.php
+++ environments/dev/backend/config/main-local.php
@@ -3,6 +3,7 @@
 $config = [
     'components' => [
         'request' => [
+            'baseUrl' => '/admin',
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',
         ],
